75ee401ae085445f63adc6aa352e9d0733f2b6e0
Jean Louis Salbego <jlsalbego@protonmail.com>
2018-10-30 11:00:48 +0100

    authorization working

--- src/app/authorization.service.ts
+++ src/app/authorization.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { tap } from 'rxjs/operators';
+
+import { environment as env } from '../environments/environment';
 
 
 @Injectable({
@@ -14,16 +16,21 @@ export class AuthorizationService {
   handleAuthorizationGrant(code) {
     return this.getAccessToken(code)
       .pipe(
+        tap(console.log),
         tap(({ access_token }: any) => localStorage.setItem('rpv_access_token', access_token))
       );
   }
 
   private getAccessToken(code) {
-    return this.http.post(`https://www.reddit.com/api/v1/access_token`, {
-      grant_type: 'authorization_code',
-      code,
-      redirect_url: 'http://localhost:4200/home'
-    });
+    return this.http.post(
+      `https://www.reddit.com/api/v1/access_token`,
+      `grant_type=authorization_code&code=${code}&redirect_uri=${env.redirect_uri}`,
+      {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/x-www-form-urlencoded',
+          'Authorization': `Basic ${btoa(`${env.clientId}:${env.secret}`)}`
+        })
+      });
   }
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+
 import { AuthorizationService } from '../authorization.service';
+import { environment as env } from '../../environments/environment';
+
 
 @Component({
   selector: 'app-home',
@@ -9,11 +12,8 @@ import { AuthorizationService } from '../authorization.service';
 })
 export class HomeComponent implements OnInit {
 
-  private readonly clientId = 'zYpfAI2IwYY0uQ';
-  private readonly secret = 'w7MuFk-NKEX-by7EyL_sQVQDGog\n';
-  private readonly random_string = 'sQVQDGog';
-
-  public authorizationUrl = `https://www.reddit.com/api/v1/authorize?client_id=${this.clientId}&response_type=code&state=${this.random_string}&redirect_uri=http://localhost:4200/home&duration=temporary&scope=read`;
+  // tslint:disable-next-line
+  public authorizationUrl = `${env.authBaseUrl}?client_id=${env.clientId}&response_type=code&state=${env.random_string}&redirect_uri=${env.redirect_uri}&duration=permanent&scope=read`;
 
   constructor(private router: Router,
               private route: ActivatedRoute,
@@ -23,13 +23,20 @@ export class HomeComponent implements OnInit {
   ngOnInit() {
     const { queryParams } = this.route.snapshot;
 
-    if (!queryParams)
-      return;
+    if (queryParams.error || !queryParams.code || queryParams.state !== env.random_string || !localStorage.getItem('rpv_access_token'))
+      this.router.navigate(['/home']);
+
+    console.log(queryParams);
+    console.log('passing', queryParams);
 
     this.authorizationService.handleAuthorizationGrant(queryParams.code)
       .subscribe(response => {
         this.router.navigate(['/pics']);
-      }, console.error);
+      }, error => {
+        console.error(error);
+        console.log('Please reload and try again.');
+      });
   }
-
 }
+
+
